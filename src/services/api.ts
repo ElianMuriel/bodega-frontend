30c9311efd5d7e9e767a1535d130b79efc9b718a
Elian Muriel <elian_2546@hotmail.com>
2025-07-10 04:20:56 -0500

    Cambio de API_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3004'; // 👈 Cambia si tu backend está en otro puerto o dominio
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3004'; // 👈 Cambia si tu backend está en otro puerto o dominio
 
 const api = axios.create({
     baseURL: API_URL,
